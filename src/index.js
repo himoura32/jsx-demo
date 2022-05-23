463952104d0c0b1407166b7306e10fb25fa50401
himoura32 <shawn.ahearn@outlook.com>
2022-05-23 16:46:05 -0400

    coded jsx basics

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = { text: 'Click me' };
+
+  return (
+    <div>
+      <label className='label' htmlFor='name'>
+        Enter name:{' '}
+      </label>
+      <input id='name' type='text' />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
