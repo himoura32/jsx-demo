91c6eec276b0d88bd0b9bbbe9db1f99f9e7f4dfc
himoura32 <shawn.ahearn@outlook.com>
2022-05-24 20:09:47 -0400

    coded App scaffold/geolocation/class component

---
+++ seasons-demo/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude:</div>;
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
