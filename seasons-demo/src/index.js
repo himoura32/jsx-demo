48fb2e5f399a3b18652a9e311ef038c99b21380f
himoura32 <shawn.ahearn@outlook.com>
2022-05-25 10:48:37 -0400

    coded state, constructor, state props, conditional

--- seasons-demo/src/index.js
+++ seasons-demo/src/index.js
@@ -2,12 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      lng: null,
+      errorMessage: '',
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
-    return <div>Latitude:</div>;
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat && this.state.lng) {
+      return (
+        <div>
+          <div>Latitude: {this.state.lat}</div>
+          <div>Longitude: {this.state.lng}</div>
+        </div>
+      );
+    }
+    return <div>Loading...</div>;
+
+    // return (
+    //   <>
+    //     <div>Latitude: {this.state.lat}</div>
+    //     <div>Longitude: {this.state.lng}</div>
+    //     {this.state.errorMessage && <div>Error: {this.state.errorMessage}</div>}
+    //   </>
+    // );
   }
 }
 
